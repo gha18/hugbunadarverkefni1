18100fdb045e1c97cdc75d8794724ea1194a2fb6
ragnheidurt <ragnheidurt3@gmail.com>
2021-10-30 15:55:09 +0000

    Setti í application

---
+++ src/main/java/project/Application.java
@@ -0,0 +1,23 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class Application extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder)
+    {
+        return applicationBuilder.sources(Application.class);
+    }
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
